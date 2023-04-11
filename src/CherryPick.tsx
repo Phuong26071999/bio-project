e1222c8f0b044ae24ea5a86ed892f9193dbbef6c
HoangPhuong <thhphuong2607@gmail.com>
2023-04-11 11:11:51 +0700

    Update 2 test cherry pick

--- src/CherryPick.tsx
+++ src/CherryPick.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function CherryPick() {
   return (
-    <div>Test CherryPick</div>
+    <>
+        <div>Test CherryPick 1</div>
+        <div>Test CherryPick 2</div>
+    </>
   )
 }
